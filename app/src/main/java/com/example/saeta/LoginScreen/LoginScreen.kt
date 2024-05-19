b4c62bd75f02ee5ece7aae295ac4e89ec6bf7665
rangertmz <andybaezarangel2002@gmail.com>
2024-05-18 22:36:46 -0600

    feature-login-screen

--- app/src/main/java/com/example/saeta/LoginScreen/LoginScreen.kt
+++ app/src/main/java/com/example/saeta/LoginScreen/LoginScreen.kt
@@ -1,7 +1,5 @@
 package com.example.saeta.LoginScreen
 
-import android.nfc.Tag
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
@@ -24,7 +21,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
