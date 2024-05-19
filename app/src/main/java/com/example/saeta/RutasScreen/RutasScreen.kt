92b34252db339d0c33a8240cd478d1e5710712fc
rangertmz <andybaezarangel2002@gmail.com>
2024-05-18 22:31:12 -0600

    feature-screens

---
+++ app/src/main/java/com/example/saeta/RutasScreen/RutasScreen.kt
@@ -0,0 +1,9 @@
+package com.example.saeta.RutasScreen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RutasScreen(){
+    Text(text = "Rutas Screen")
+}
