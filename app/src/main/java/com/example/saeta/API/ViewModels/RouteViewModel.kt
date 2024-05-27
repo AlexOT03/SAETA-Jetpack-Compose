300709c22b3ad8e8d6c47603780bda7fd9f14d6c
rangertmz <andybaezarangel2002@gmail.com>
2024-05-27 10:40:35 -0600

    fix-remove-innecesary-functions

--- app/src/main/java/com/example/saeta/API/ViewModels/RouteViewModel.kt
+++ app/src/main/java/com/example/saeta/API/ViewModels/RouteViewModel.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.viewModelScope
 import com.example.saeta.API.Data
 import com.example.saeta.API.Route
 import com.example.saeta.API.RouteApi
+import com.example.saeta.API.Trip
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
@@ -16,6 +17,9 @@ import java.io.IOException
 class RouteViewModel : ViewModel() {
     private val _routes = MutableLiveData<List<Route>>()
     val routes: LiveData<List<Route>> get() = _routes
+    private val _route = MutableLiveData<Route>()
+    val route: LiveData<Route> get() = _route
+
 
     private val _error = MutableLiveData<String?>()
     val error: LiveData<String?> get() = _error
@@ -47,4 +51,31 @@ class RouteViewModel : ViewModel() {
             }
         }
     }
+
+    fun fetchRouteById(id: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val response = RouteApi.apiService.getRouteById(id).execute()
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        _route.postValue(it)
+                        _error.postValue(null)
+                    } ?: run {
+                        _error.postValue("No data available")
+                    }
+                } else {
+                    _error.postValue("Failed to fetch data")
+                }
+            } catch (e: HttpException) {
+                _error.postValue("HTTP error: ${e.message}")
+            } catch (e: IOException) {
+                _error.postValue("Network error: ${e.message}")
+            } catch (e: Exception) {
+                _error.postValue("Unexpected error: ${e.message}")
+            }
+        }
+    }
+
+
+
 }
